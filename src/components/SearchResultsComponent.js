092e58bccf5b0b45a72b89ac25a3cb4ea011148a
Mithun <mithunjmistry@gmail.com>
2018-03-08 21:51:35 -0500

    sort by on search result UI created

--- src/components/SearchResultsComponent.js
+++ src/components/SearchResultsComponent.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import {Grid, Row, Col, ListGroup} from 'react-bootstrap';
+import {Grid, Row, Col, ListGroup, DropdownButton, MenuItem} from 'react-bootstrap';
 
 class SearchResultsComponent extends React.Component{
+
+    state = {
+      sortBySelected: "Relevance",
+      sortByOptions: ["Price: Low to High", "Price: High to Low", "New"]
+    };
 
     componentDidMount(){
         let category = this.props.match.params.category;
@@ -21,12 +26,74 @@ class SearchResultsComponent extends React.Component{
         }
     }
 
+    sortByChange = (selectedSortBy) => {
+        switch(selectedSortBy){
+            case 'Price: Low to High':
+                this.setState((prevState) => {
+                    return {
+                        sortByOptions:
+                            prevState.sortByOptions.concat(prevState.sortBySelected).filter((menuItem) => (
+                                menuItem !== "Price: Low to High"
+                            )),
+                        sortBySelected: "Price: Low to High"
+                    }
+                });
+                break;
+            case 'Price: High to Low':
+                this.setState((prevState) => {
+                    return {
+                        sortByOptions:
+                            prevState.sortByOptions.concat(prevState.sortBySelected).filter((menuItem) => (
+                                menuItem !== "Price: High to Low"
+                            )),
+                        sortBySelected: "Price: High to Low"
+                    }
+                });
+                break;
+            case 'New':
+                this.setState((prevState) => {
+                    return {
+                        sortByOptions:
+                            prevState.sortByOptions.concat(prevState.sortBySelected).filter((menuItem) => (
+                                menuItem !== "New"
+                            )),
+                        sortBySelected: "New"
+                    }
+                });
+                break;
+            default:
+                this.setState((prevState) => {
+                    return {
+                        sortByOptions:
+                            prevState.sortByOptions.concat(prevState.sortBySelected).filter((menuItem) => (
+                                menuItem !== "Relevance"
+                            )),
+                        sortBySelected: "Relevance"
+                    }
+                });
+                break;
+        }
+    };
+
     render() {
         return (
             <Grid>
                 <Row>
                     <Col lg={10} md={10}>
-                        <p>Content</p>
+                        <div>
+                            <label className={'sort-by-label'}>Sort By - </label>
+                            <DropdownButton
+                                bsStyle={'default'}
+                                title={this.state.sortBySelected}
+                                key={'sortBy'}
+                                id={`dropdown-sort-by`}
+                                className={'btn-sm'}
+                            >
+                                {this.state.sortByOptions.map((sortByOption) => (
+                                    <MenuItem key={sortByOption} onClick={() => this.sortByChange(sortByOption)}>{sortByOption}</MenuItem>
+                                ))}
+                            </DropdownButton>
+                        </div>
                     </Col>
                     <Col lg={2} md={2}>
                         <p>Advanced Filters</p>
