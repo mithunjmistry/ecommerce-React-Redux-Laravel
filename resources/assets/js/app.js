a7c9fe02daeeaafe57e7d719d83564ae23d324ce
Mithun <mithunjmistry@gmail.com>
2018-04-02 00:27:06 -0400

    user status check added on first-time web app load

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,6 +8,10 @@ import '../sass/app.scss';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {addToCart} from "./actions/shoppingCart";
 import {imageWatch} from "./components/image";
+import axios from "./api/axiosInstance";
+import {getUserAPI} from "./api/apiURLs";
+import {loginUser, logoutUser} from "./actions/authentication";
+import {ACCESS_TOKEN} from "./api/strings";
 
 const store = configureStore();
 console.log(store.getState());
@@ -33,6 +37,18 @@ const product = {
     productID: 1
 };
 store.dispatch(addToCart(product));
+
+// initial load, check if user is logged in
+const access_token = window.localStorage.getItem(ACCESS_TOKEN);
+const headers = {Accept: "application/json", Authorization: `Bearer ${access_token}`};
+axios.get(getUserAPI, {headers})
+    .then((response) => {
+        store.dispatch(loginUser());
+    })
+    .catch((error) => {
+        window.localStorage.removeItem(ACCESS_TOKEN);
+        store.dispatch(logoutUser());
+    });
 
 const appRoot = document.getElementById('app');
 ReactDOM.render(jsx, appRoot);
